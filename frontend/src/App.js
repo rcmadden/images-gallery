c739d42fd80e6346c29072cadd45d434551abf82
rcmadden <dataengineer007@gmail.com>
2022-07-27 17:40:19 -0700

    feat: Add and style search button and form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
+import Search from './components/Search';
+
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target[0].value);
+}
 
 function App() {
   return (
     <div className="App">
       <Header title="Images Gallery"/>
+      <Search handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
