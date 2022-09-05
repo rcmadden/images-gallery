9391787d27b94504b50fe88c3fda387375d8fc9e
rcmadden <dataengineer007@gmail.com>
2022-09-05 13:35:04 -0700

    feat: Basic images enpoint with get and post methods

--- api/main.py
+++ api/main.py
@@ -1,8 +1,13 @@
+from crypt import methods
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -18,6 +23,7 @@ CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+# insert_test_document()
 
 
 @app.route("/new-image")
@@ -32,6 +38,22 @@ def new_image():
 
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        #read images frm the databse
+        images = images_collection.find({})
+        return jsonify([img for img in images])
+    if request.method == "POST":
+        #save image in the database
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
