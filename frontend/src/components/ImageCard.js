1e876f930e9731d582cd133e717e5dd143943b00
rcmadden <dataengineer007@gmail.com>
2022-10-01 15:22:17 -0700

    feat: Add image and author info to card

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Nav } from 'react-bootstrap';
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No Author Name';
+  const authorPortfolioURL = image.user?.portfolio_url;
+  const imageLocation = image.location?.name;
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
         <Card.Title>{image.title?.toUpperCase()}</Card.Title>
+        <Card.Subtitle>{imageLocation}</Card.Subtitle>
         <Card.Text>{image.description || image.alt_description}</Card.Text>
         <Button variant="primary" onClick={() => deleteImage(image.id)}>
           Delete
@@ -17,6 +22,14 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           </Button>
         )}
       </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        {authorPortfolioURL && (
+          <Nav.Link href={authorPortfolioURL} target="_blank">
+            {authorName}
+          </Nav.Link>
+        )}
+        {!authorPortfolioURL && authorName}
+      </Card.Footer>
     </Card>
   );
 };
